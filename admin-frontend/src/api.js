a9f256b037fb30cd020b97d0eab99eb3f150b277
Chirantan Jana <97895958+chirantan1@users.noreply.github.com>
2025-06-04 10:52:57 +0530

    Update api.js

--- admin-frontend/src/api.js
+++ admin-frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// Adjust this to your backend URL
+// Pointing to deployed backend on Render
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/admin',
+  baseURL: 'https://admin-w4u5.onrender.com/api/admin',
 });
 
 export default api;
